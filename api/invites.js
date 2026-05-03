ad51ff09195773c12a2d3ccb55dea6fb68202da9
avzhidkov <zhidkov.spb@gmail.com>
2026-05-03 17:37:55 +0300

    инвайты

--- api/invites.js
+++ api/invites.js
@@ -98,10 +98,10 @@ export default async function handler(req, res) {
         uses: (current?.uses || 0) + 1,
       })
       .eq("code", code)
-      .select().single();
+      .select();
 
     if (error) return res.status(500).json({ error: error.message });
-    return res.status(200).json(data);
+    return res.status(200).json(data?.[0] || {});
   }
 
   res.status(405).json({ error: "Method not allowed" });
