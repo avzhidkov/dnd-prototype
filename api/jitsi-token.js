63177579315c4adb037b5c12318b90006b2b6c54
avzhidkov <zhidkov.spb@gmail.com>
2026-05-04 03:04:44 +0300

    джитси

--- api/jitsi-token.js
+++ api/jitsi-token.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
+import { createClient } from '@supabase/supabase-js';
 
 export default async function handler(req, res) {
-  res.json({ ok: true, jwt: typeof jwt });
+  res.json({ ok: true, jwt: typeof jwt, supabase: typeof createClient });
 }
